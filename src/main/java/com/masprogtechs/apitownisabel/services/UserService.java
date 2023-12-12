4133a0a76a2b686d7c51bf043e7521438bfe59f7
masprog2022 <masprog2022@gmail.com>
2023-12-13 00:31:37 +0100

    :sparkles: feat: adicionando passwordEncoder no metodo save

--- src/main/java/com/masprogtechs/apitownisabel/services/UserService.java
+++ src/main/java/com/masprogtechs/apitownisabel/services/UserService.java
@@ -6,6 +6,7 @@ import com.masprogtechs.apitownisabel.models.User;
 import com.masprogtechs.apitownisabel.repositories.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -14,10 +15,12 @@ import org.springframework.transaction.annotation.Transactional;
 public class UserService {
 
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
     public User save(User user){
         try{
-          return userRepository.save(user);
+            user.setPassword(passwordEncoder.encode(user.getPassword()));
+            return userRepository.save(user);
         }catch (DataIntegrityViolationException ex){
             throw new UsernameUniqueViolationException(String.format("Username %s já registado no sistema", user.getUsername()));
         }
